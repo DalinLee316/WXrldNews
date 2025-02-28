78e8883df08b585f538208309fe6304e4c0bf6cc
DalinLee316 <crane316285@gmail.com>
2025-02-28 21:39:04 +0100

    Add files via upload

---
+++ main.py
@@ -0,0 +1,45 @@
+import feedparser
+from telegram import Bot
+import schedule
+import time
+import asyncio
+
+# Токен и ID чата
+TOKEN = "7738677308:AAFRGuARwQq8DCs7AksRw8UB3jPHp-39xE0"
+CHAT_ID = 6754357775  
+
+# RSS-лента New York Times
+RSS_URL = "http://rss.nytimes.com/services/xml/rss/nyt/HomePage.xml"
+
+# Создание бота
+bot = Bot(token=TOKEN)
+
+# Функция для отправки новостей
+async def send_news():
+    print("Функция send news() вызвана")
+    try:
+        feed = feedparser.parse(RSS_URL)
+        if not feed.entries:
+            print("Нет свежих новостей")
+            return
+
+        news_list = [entry.title for entry in feed.entries[:5]]
+        message = "\n".join(news_list)
+
+        await bot.send_message(chat_id=6754357775, text=message)
+        print("Новости отправлены")
+
+    except Exception as e:
+        print(f"Ошибка: {e}")
+
+def run_asyncio_job():
+    asyncio.run(send_news())
+
+# Запускаем `send_news()` каждые 10 минут
+schedule.every(10).minutes.do(run_asyncio_job)
+
+# Бесконечный цикл для работы schedule
+while True:
+    print("\rЖду новости...", end="", flush=True)
+    schedule.run_pending()
+    time.sleep(1)
